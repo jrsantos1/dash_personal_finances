5d88e46bb752910327a95264cfeb00a428abb111
jrsantos1 <jhonatanjonh@outlook.com>
2023-03-20 13:21:10 -0300

    add testes arquivo app2

--- app2/app.py
+++ app2/app.py
@@ -2,6 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 from dash import dcc
 from layout import conteudo
+from layout_novo import conteudo as new_content
 
 estilos = ["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", "https://fonts.googleapis.com/icon?family=Material+Icons", dbc.themes.BOOTSTRAP]
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
@@ -9,7 +10,7 @@ dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4
 app = dash.Dash(__name__, external_stylesheets=estilos + [dbc_css])
 
 app.layout = dbc.Container([
-   conteudo
+   new_content
 ], fluid=True)
 
 if __name__ == '__main__':
